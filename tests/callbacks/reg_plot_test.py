b58736a01961090768a209a6fccf1a8292d6d450
Robert Bock <robertbock.98@googlemail.com>
2023-05-11 11:43:34 +0200

    Adjusted Tests.

--- tests/callbacks/reg_plot_test.py
+++ tests/callbacks/reg_plot_test.py
@@ -6,10 +6,10 @@ from pytorch_lightning.loggers import TensorBoardLogger
 
 import pytorch_lightning as pl
 
+CMAPS = ["rainbow", "Reds", matplotlib.colormaps.get_cmap("plasma")]
 
-@pytest.mark.parametrize(
-    "cmap", ["rainbow", "Reds", matplotlib.colormaps.get_cmap("plasma")]
-)
+
+@pytest.mark.parametrize("cmap", CMAPS)
 def test_regression_callback(linear_thunder_storing_model, xor_dataloader, cmap):
     rpc = RegressionPlotCallback(cmap=cmap)
     tb = TensorBoardLogger(".")
@@ -20,3 +20,17 @@ def test_regression_callback(linear_thunder_storing_model, xor_dataloader, cmap)
     )
 
     trainer.fit(linear_thunder_storing_model, xor_dataloader, xor_dataloader)
+
+
+@pytest.mark.parametrize("cmap", CMAPS)
+def test_call_with_wrong_model(linear_model, xor_dataloader, cmap):
+    rpc = RegressionPlotCallback(cmap=cmap)
+    tb = TensorBoardLogger(".")
+    trainer = pl.Trainer(
+        callbacks=[rpc],
+        logger=tb,
+        max_epochs=16,
+    )
+
+    with pytest.raises(ValueError):
+        trainer.fit(linear_model, xor_dataloader, xor_dataloader)
